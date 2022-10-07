c084994f6b4711cb8a29344e76c3c5649ac2d22e
rachid <chamirachid1@gmail.com>
2022-10-07 11:32:55 +0100

    chore: rename LoaderI to QGBLoaderI

--- x/qgb/orchestrator/loader.go
+++ x/qgb/orchestrator/loader.go
@@ -2,7 +2,7 @@ package orchestrator
 
 import "github.com/celestiaorg/celestia-app/x/qgb/types"
 
-type LoaderI interface {
+type QGBLoaderI interface {
 	Start() error
 	Stop() error
 	GetDataCommitmentConfirms(nonce uint64) ([]types.MsgDataCommitmentConfirm, error)
@@ -13,44 +13,44 @@ type LoaderI interface {
 	GetValsetConfirmByOrchestratorAddress(nonce uint64, orch string) (types.MsgValsetConfirm, error)
 }
 
-type InMemoryLoader struct {
+type InMemoryQGBLoader struct {
 	store QGBInMemoryStore
 }
 
-var _ LoaderI = &InMemoryLoader{}
+var _ QGBLoaderI = &InMemoryQGBLoader{}
 
-func NewInMemoryLoader(store QGBInMemoryStore) *InMemoryLoader {
-	return &InMemoryLoader{store: store}
+func NewInMemoryLoader(store QGBInMemoryStore) *InMemoryQGBLoader {
+	return &InMemoryQGBLoader{store: store}
 }
 
-func (loader InMemoryLoader) Start() error {
+func (loader InMemoryQGBLoader) Start() error {
 	return nil
 }
 
-func (loader InMemoryLoader) Stop() error {
+func (loader InMemoryQGBLoader) Stop() error {
 	return nil
 }
 
-func (loader InMemoryLoader) GetDataCommitmentConfirms(nonce uint64) ([]types.MsgDataCommitmentConfirm, error) {
+func (loader InMemoryQGBLoader) GetDataCommitmentConfirms(nonce uint64) ([]types.MsgDataCommitmentConfirm, error) {
 	return loader.store.GetDataCommitmentConfirms(nonce)
 }
 
-func (loader InMemoryLoader) GetDataCommitmentConfirmByOrchestratorAddress(nonce uint64, orch string) (types.MsgDataCommitmentConfirm, error) {
+func (loader InMemoryQGBLoader) GetDataCommitmentConfirmByOrchestratorAddress(nonce uint64, orch string) (types.MsgDataCommitmentConfirm, error) {
 	return loader.store.GetDataCommitmentConfirmByOrchestratorAddress(nonce, orch)
 }
 
-func (loader InMemoryLoader) GetDataCommitmentConfirmByEthereumAddress(nonce uint64, ethAddr string) (types.MsgDataCommitmentConfirm, error) {
+func (loader InMemoryQGBLoader) GetDataCommitmentConfirmByEthereumAddress(nonce uint64, ethAddr string) (types.MsgDataCommitmentConfirm, error) {
 	return loader.store.GetDataCommitmentConfirmByEthereumAddress(nonce, ethAddr)
 }
 
-func (loader InMemoryLoader) GetValsetConfirms(nonce uint64) ([]types.MsgValsetConfirm, error) {
+func (loader InMemoryQGBLoader) GetValsetConfirms(nonce uint64) ([]types.MsgValsetConfirm, error) {
 	return loader.store.GetValsetConfirms(nonce)
 }
 
-func (loader InMemoryLoader) GetValsetConfirmByEthereumAddress(nonce uint64, ethAddr string) (types.MsgValsetConfirm, error) {
+func (loader InMemoryQGBLoader) GetValsetConfirmByEthereumAddress(nonce uint64, ethAddr string) (types.MsgValsetConfirm, error) {
 	return loader.store.GetValsetConfirmByEthereumAddress(nonce, ethAddr)
 }
 
-func (loader InMemoryLoader) GetValsetConfirmByOrchestratorAddress(nonce uint64, orch string) (types.MsgValsetConfirm, error) {
+func (loader InMemoryQGBLoader) GetValsetConfirmByOrchestratorAddress(nonce uint64, orch string) (types.MsgValsetConfirm, error) {
 	return loader.store.GetValsetConfirmByOrchestratorAddress(nonce, orch)
 }
